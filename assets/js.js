36e2b0dd9765b820359e8b8f0729794e703b26d7
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-11-26 10:57:30 -0500

    initial commit

---
+++ assets/js.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+	$("#red").on("click", function(){
+		$("body").css("background", "red");
+	});
+
+	$("#blue").on("click", function(){
+		$("body").css("background", "blue");
+	});
+
+	$("#yellow").on("click", function(){
+		$("body").css("background", "yellow");
+	});
+
+	$("#green").on("click", function(){
+		$("body").css("background", "green");
+	});
+
+});
