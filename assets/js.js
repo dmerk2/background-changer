78adb3db4fcb1f7589b98c5dac467a281366fe9a
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-11-26 11:54:51 -0500

    adding random colors button

--- assets/js.js
+++ assets/js.js
@@ -16,4 +16,14 @@ $(document).ready(function(){
 		$("body").css("background", "green");
 	});
 
+	$("#random").on("click", function(){
+		
+		for(var i = 0; i < 3; i++){
+			var rgb = [];
+			var rgb = rgb.push(Math.floor(Math.random() * 255));
+		
+		var colors = ['red', 'blue', 'yellow', 'green', 'purple', rgb];
+    	$("body").css('background-color', colors[Math.floor(Math.random() * colors.length)]);
+		};
+	});
 });
